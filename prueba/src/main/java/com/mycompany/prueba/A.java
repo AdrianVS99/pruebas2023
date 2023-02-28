43a38f7eeefc73d090aa970cc4433199df423eb7
Seven <Seven@10.81.104.201>
2023-02-28 14:16:06 +0100

    Nueva clase A añadida

---
+++ prueba/src/main/java/com/mycompany/prueba/A.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author Seven
+ */
+public class A
+{
+    private String A;
+
+    public A(String A)
+    {
+        this.A = A;
+    }
+
+    public String getA()
+    {
+        return A;
+    }
+
+    public void setA(String A)
+    {
+        this.A = A;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "A{" + "A=" + A + '}';
+    }
+    
+    
+}
